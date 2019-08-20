90914a2f709221d72b62880db3b431f77dfb3300
Monta Kigule <monta_kigule@rcomext.com>
2019-08-21 00:11:25 +0300

    Lesson 5

---
+++ Lesson5/Triangle.java
@@ -0,0 +1,15 @@
+public class Triangle {
+
+    public String isTriangle(int a, int b, int c) {
+        if (a + b > c && b + c > a && c + a > b) {
+            return "YES";
+        } else {
+            return "NO";
+        }
+    }
+
+    public void myMethod() {
+        System.out.println("Something");
+    }
+
+}
