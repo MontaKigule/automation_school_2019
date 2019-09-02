5d0db9898035326205514f1db07b58ba221b874c
MontaKigule <41991636+MontaKigule@users.noreply.github.com>
2019-09-02 20:12:28 +0300

    Update ArithmeticAverageTest.java

--- Lesson7/ArithmeticAverageTest.java
+++ Lesson7/ArithmeticAverageTest.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ArithmeticAverageTest {
@@ -14,45 +13,31 @@ public class ArithmeticAverageTest {
 
     }
 
-    @BeforeEach
-    public static void initEach() {
-        a = -5;
-        b = 12;
-    }
-
     @Test
     public void isArithmeticAver()
     {
-        this.initEach();
+        a = -5;
+        b = 12;
         double expected = 15;
         double actual = aa.theArithmeticAverageOfAllNumbers(a, b);
         Assertions.assertEquals(expected, actual, "Expected is Fail");
     }
 
-    @BeforeEach
-    public static void initEach2() {
-        a = 0;
-        b = 9;
-    }
-
     @Test
     public void isArithmeticAver2()
     {
-        this.initEach2();
+        a = 0;
+        b = 9;
         double expected = 6;
         double actual = aa.theArithmeticAverageOfAllNumbers(a, b);
         Assertions.assertEquals(expected, actual, "Arithmetic average of all numbers from the interval which are divisible by 3.");
-    }
-    @BeforeEach
-    public static void initEach3() {
-        a = 0;
-        b = 0;
     }
 
     @Test
     public void isArithmeticAver3()
     {
-        this.initEach3();
+        a = 0;
+        b = 0;
         double expected = 0;
         double actual = aa.theArithmeticAverageOfAllNumbers(a, b);
         Assertions.assertEquals(expected, actual, "Arithmetic average of all numbers from the interval does not divisible by 3.");
